bdb9d63adf4186979f75e9e76470ced00b131ffc
JimmyPop22 <james.pyne22@gmail.com>
2025-08-28 16:45:21 +0000

    Finished ex0101

---
+++ src/ex0101.cpp
@@ -0,0 +1,22 @@
+//promt the user for two integers and run some arithmatic
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x, y;
+
+cout <<"Enter X:";
+cin >> x;
+
+cout <<"Enter Y:";
+cin >> y;
+
+
+cout << "x + y = " << (x + y) << endl; 
+cout << "x - y = " << (x - y) << endl;
+cout << "x * y = " << (x * y) << endl;
+cout << "x / y = " << (x / y) << endl;
+
+    return 0;
+}
